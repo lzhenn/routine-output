61bef74992bd212cbda82116304d2e2ad8fe2ad7
novarizark <lzhenn@mail2.sysu.edu.cn>
2020-07-15 12:01:13 +0800

    20200715

--- fetch_data.sh
+++ fetch_data.sh
@@ -6,8 +6,12 @@ TODAY=$(date +"%Y%m%d")
 SPELLCASTER_FIG=/disk/hq247/yhuangci/lzhenn/workspace/spellcaster-local/fig/realtime.*
 ROUTINE_AIM=/disk/hq247/yhuangci/lzhenn/workspace/routine-output/spellcaster/
 
+SPELLCASTER_TAR=/disk/hq247/yhuangci/lzhenn/workspace/spellcaster-local/features.tar.gz
+ROUTINE_AIM=/disk/hq247/yhuangci/lzhenn/workspace/routine-output/spellcaster/
+
 # execute
 cp $SPELLCASTER_FIG $ROUTINE_AIM 
+cp $SPELLCASTER_TAR $ROUTINE_AIM 
 
 # refresh git
 git add *
